09261aa6443550ea103f5638166057fa2b079bea
johnitopaisah <johnitopaisah@gmail.com>
2025-04-23 04:21:19 +0200

    added the cluster file

--- terraform-files/variables.tf
+++ terraform-files/variables.tf
@@ -24,7 +24,7 @@ variable "private_subnet_cidr_blocks" {
   default = ["12.0.1.0/24", "12.0.2.0/24", "12.0.3.0/24"]
 }
 
-variable "private_subnet_cidr_blocks" {
+variable "public_subnet_cidr_blocks" {
   default = ["12.0.101.0/24", "12.0.102.0/24", "12.0.103.0/24"]
 }
 
